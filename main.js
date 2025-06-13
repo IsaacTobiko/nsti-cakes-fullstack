f56ecd63c57bd402087a940566c11f4de70aa8a9
AYZOH <ayzoh@kali.AYZOH>
2025-06-13 01:17:18 -0400

    Added dev.html and main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+//Data types include;
+const name = "Isaac"; //string.
+const age = 40; //number.
+const month = 2.5; //number *There is no decimal in Javascript.
+const isCool = true; //boolean.
+const x = null; //null
+const y = undefined;
+let z;
+
+console.log(typeof x);

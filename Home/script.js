72808ee88a4493cd484d39791798d95421830fd6
AYZOH <tobikoisaack@gmail.com>
2025-07-15 08:58:06 +0300

    Added Home/script.js

--- Home/script.js
+++ Home/script.js
@@ -1,3 +1,22 @@
-const s = "Hello world!";
+const todos = [
+  {
+    id: 1,
+    text: "Take the trash",
+    isCompleted: true,
+  },
 
-console.log(s.toUpperCase());
+  {
+    id: 2,
+    text: "Visit yuh friend",
+    isCompleted: true,
+  },
+
+  {
+    id: 3,
+    text: "Cook",
+    isCompleted: false,
+  },
+];
+
+const todoJSON = JSON.stringify(todos);
+console.log(todoJSON);

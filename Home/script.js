7b8913c19a20857d3003c2cfa1cdca9c57247621
AYZOH <tobikoisaack@gmail.com>
2025-07-16 13:51:16 +0300

    Home/script.js

--- Home/script.js
+++ Home/script.js
@@ -1,22 +1,23 @@
-const todos = [
-  {
-    id: 1,
-    text: "Take the trash",
-    isCompleted: true,
-  },
+//Class
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+  }
 
-  {
-    id: 2,
-    text: "Visit yuh friend",
-    isCompleted: true,
-  },
+  getBirthYear() {
+    return this.dob.getFullYear();
+  }
 
-  {
-    id: 3,
-    text: "Cook",
-    isCompleted: false,
-  },
-];
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
 
-const todoJSON = JSON.stringify(todos);
-console.log(todoJSON);
+//Instantiate object
+const person1 = new Person("John", "Doe", "4-3-1980");
+const person2 = new Person("Mary", "Smith", "3-6-1970");
+
+console.log(person2.getFullName());
+console.log(person1);

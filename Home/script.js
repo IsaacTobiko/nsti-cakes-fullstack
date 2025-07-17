8551493fcde4c4dc3cfa5194ac8ac910d7ae1b37
AYZOH <tobikoisaack@gmail.com>
2025-07-17 21:25:02 +0300

    Added Home/script.js

--- Home/script.js
+++ Home/script.js
@@ -1,23 +1,6 @@
-//Class
-class Person {
-  constructor(firstName, lastName, dob) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.dob = new Date(dob);
-  }
-
-  getBirthYear() {
-    return this.dob.getFullYear();
-  }
-
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
-
-//Instantiate object
-const person1 = new Person("John", "Doe", "4-3-1980");
-const person2 = new Person("Mary", "Smith", "3-6-1970");
-
-console.log(person2.getFullName());
-console.log(person1);
+const btn = document.querySelector(".btn");
+btn.addEventListener("mouseout", (e) => {
+  e.preventDefault();
+  btn.style.background = "red";
+  document.querySelector("body").classList.add("copy");
+});

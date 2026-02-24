e52687a2cf76286e2f5833ee9987290ac715e3df
AYZOH <tobikoisaack@gmail.com>
2026-02-24 12:05:21 +0300

    Add client and server folders, remove old unused folders

--- Home/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector(".btn");
-btn.addEventListener("mouseout", (e) => {
-  e.preventDefault();
-  btn.style.background = "red";
-  document.querySelector("body").classList.add("copy");
-});
